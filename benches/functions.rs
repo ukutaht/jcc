b6e07bc24cc48858f6915e43ed67288ee9de238f
Uku Taht <uku.taht@gmail.com>
2017-01-30 14:04:27 +0000

    Add bench for function with arguments

--- benches/functions.rs
+++ benches/functions.rs
@@ -7,3 +7,8 @@ use test::Bencher;
 fn bench_named_function(b: &mut Bencher) {
     b.iter(|| jcc::transform("function hello() { var a = 1 }"));
 }
+
+#[bench]
+fn bench_named_function_with_arguments(b: &mut Bencher) {
+    b.iter(|| jcc::transform("function hello(a, b) { var c = 1 }"));
+}
