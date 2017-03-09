21d6bf822c81a5d11f2b2c2805022f4fbf88c8bf
Uku Taht <uku.taht@gmail.com>
2017-03-09 16:51:20 +0000

    Make first esprima test pass(!)

--- tests/json/mod.rs
+++ tests/json/mod.rs
@@ -1,32 +1,56 @@
 extern crate jcc;
 
+use std;
 use jcc::syntax::ast::*;
 use serde_json::value::Value;
 
-fn expression(node: &Value) -> Option<Expression> {
-    let obj = node.as_object().unwrap();
-    match obj.get("type").unwrap().as_str().unwrap() {
-        "FunctionExpression" => {
-            None
+pub type Result<T> = std::result::Result<T, ()>;
+
+fn expect_string<'a>(node: &'a Value, key: &str) -> &'a str {
+    node.as_object().unwrap().get(key).unwrap().as_str().unwrap()
+}
+
+fn expect_value<'a>(node: &'a Value, key: &str) -> &'a Value {
+    node.as_object().unwrap().get(key).unwrap()
+}
+
+fn binary_expression(node: &Value) -> Result<Expression> {
+    match expect_string(node, "operator") {
+        "==" => {
+            let left = expression(expect_value(node, "left"))?;
+            let right = expression(expect_value(node, "right"))?;
+            Ok(Expression::Binary(BinOp::EqEq, Box::new(left), Box::new(right)))
         },
-        "Identifier" => {
-            None
-        }
-        _ => None
+        _ => Err(())
     }
 }
 
-fn statement(node: &Value) -> Option<Statement> {
+fn expression(node: &Value) -> Result<Expression> {
+    match expect_string(node, "type") {
+        "FunctionExpression" => {
+            Err(())
+        },
+        "Identifier" => {
+            Ok(Expression::Identifier(expect_string(node, "name").to_owned()))
+        }
+        "BinaryExpression" => {
+            binary_expression(node)
+        },
+        _ => Err(())
+    }
+}
+
+fn statement(node: &Value) -> Result<Statement> {
     let obj = node.as_object().unwrap();
     match obj.get("type").unwrap().as_str().unwrap() {
         "ExpressionStatement" => {
             expression(obj.get("expression").unwrap()).map(Statement::Expression)
         }
-        _ => None
+        _ => Err(())
     }
 }
 
-fn statement_list_item(node: &Value) -> Option<StatementListItem> {
+fn statement_list_item(node: &Value) -> Result<StatementListItem> {
     statement(node).map(StatementListItem::Statement)
 }
 
@@ -36,8 +60,8 @@ pub fn parse_program(value: &Value) -> Program {
 
     for node in body {
         match statement_list_item(node) {
-            Some(item) => statement_list_items.push(item),
-            None => continue
+            Ok(item) => statement_list_items.push(item),
+            Err(_) => continue
         }
     }
 
